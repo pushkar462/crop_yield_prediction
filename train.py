008780ead81a3eaab35c64a398bef27bcf9e25cc
Pushkar Sharma <pushkarsharma.sharma2004@gmail.com>
2026-02-23 21:09:22 +0530

    Added global seed for reproducibility

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import joblib
 import os
+import random
+random.seed(42)
+np.random.seed(42)
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
